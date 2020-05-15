3746937b56eca6bef15daa6247d162f48928d85d
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2020-05-14 22:14:27 -0500

    mvp

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
 
 const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
+server.use(logger);
+
+server.use('/api/post', postRouter);
+server.use('/api/user', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +20,9 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} ${req.url}`);
+  next();
+}
 
 module.exports = server;
